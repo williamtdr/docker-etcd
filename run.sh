74710586946c24f84b4ade3d1d16fdc6e2570551
William Teder <wteder@hydreon.com>
2017-08-23 09:55:23 -0700

    etcd3 ipv6 resolve fix

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Check for $CLIENT_URLS
 if [ -z ${CLIENT_URLS+x} ]; then
-        CLIENT_URLS="http://0.0.0.0:4001,http://0.0.0.0:2379"
+        CLIENT_URLS="http://etcd:4001,http://etcd:2379"
         echo "Using default CLIENT_URLS ($CLIENT_URLS)"
 else
         echo "Detected new CLIENT_URLS value of $CLIENT_URLS"
@@ -9,13 +9,13 @@ fi
 
 # Check for $PEER_URLS
 if [ -z ${PEER_URLS+x} ]; then
-        PEER_URLS="http://0.0.0.0:7001,http://0.0.0.0:2380"
+        PEER_URLS="http://etcd:7001,http://etcd:2380"
         echo "Using default PEER_URLS ($PEER_URLS)"
 else
         echo "Detected new PEER_URLS value of $PEER_URLS"
 fi
 
-ETCD_CMD="/bin/etcd -data-dir=/data -listen-peer-urls=${PEER_URLS} -listen-client-urls=${CLIENT_URLS} $*"
+ETCD_CMD="/bin/etcd --data-dir=/data --listen-peer-urls=${PEER_URLS} --listen-client-urls=${CLIENT_URLS} --advertise-client-urls=http://etcd:2379,http://etcd:4001 $*"
 echo -e "Running '$ETCD_CMD'\nBEGIN ETCD OUTPUT\n"
 
 exec $ETCD_CMD
